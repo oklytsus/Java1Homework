822b41c6e8f13201467e59e85835fc865942e75e
oklytsus <oksana.lytsus@gmail.com>
2023-11-21 15:22:16 +0100

    Simple Java Classes

---
+++ src/main/java/org/softserve/academy/PriceVat.java
@@ -0,0 +1,22 @@
+package org.softserve.academy;
+
+import java.util.Scanner;
+/**
+ * Create a program that asks the user for the price of a product excluding VAT
+ * and calculates the final cost including VAT (add 20% to the initial price).
+ * Display both prices — excluding VAT and including VAT.
+ * Use the Scanner to enter the initial price.
+ */
+public class PriceVat {
+    public static void main(String[] args) {
+        double price, priceVat;
+        final double VAT = 0.2;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enetre your price:");
+        price= sc.nextDouble();
+        priceVat=price+price*VAT;
+        System.out.println("Cost excluding  VAT is: " + price);
+        System.out.print("Cost including  VAT is: ");
+        System.out.printf(String.valueOf(priceVat));
+    }
+}
