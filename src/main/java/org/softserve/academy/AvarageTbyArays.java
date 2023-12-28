8def8ecc1970ea6ad34dcdf3c6f6f1edb2f2c15e
oklytsus <oksana.lytsus@gmail.com>
2023-12-28 03:25:21 +0100

    Added Age Verification for Incorrect values

--- src/main/java/org/softserve/academy/AvarageTbyArays.java
+++ src/main/java/org/softserve/academy/AvarageTbyArays.java
@@ -9,14 +9,14 @@ public class AvarageTbyArays
     {
         float averageT, sum=0;
         int days=5;
-        float temperature[] = new float[days];
+        float[] temperatures = new float[days];
 
 
         for (int i=1; i<=days; i++ ){
             System.out.println("Please input temperature for day" + i);
-            temperature[i-1]= sc.nextFloat();
-            sum+=temperature[i-1];
-        };
+            temperatures[i-1]= sc.nextFloat();
+            sum+=temperatures[i-1];
+        }
 
         averageT=sum/days;
         System.out.print("The average temperature for " +days+ " days is : ");
